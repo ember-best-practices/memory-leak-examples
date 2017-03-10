12363222eaf91450e1bbf94f6f19210dd9199a71
Trent Willis <twillis@linkedin.com>
2017-03-10 09:22:06 -0800

    Add simple example of callback leak for event listener

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -14,5 +14,11 @@ export default Ember.Controller.extend({
 
     // Save the application controller for use with other things
     this.get('storage').set('application-controller', this);
+  },
+
+  actions: {
+    onScroll() {
+      this.set('scrollDistance', window.scrollY);
+    }
   }
 });
