fa6ee6486d8340a85e694e62fbe1255522e8fe94
Trent Willis <twillis@linkedin.com>
2017-03-31 11:39:18 -0700

    Add exercise 1

--- tests/unit/services/shared-storage-test.js
+++ tests/unit/services/shared-storage-test.js
@@ -2,7 +2,27 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:shared-storage', 'Unit | Service | shared storage');
 
-test('it exists', function(assert) {
+test('smoke test', function(assert) {
   let service = this.subject();
-  assert.ok(service);
+
+  service.set('subject', service);
+  service.set('falsy', '');
+
+  assert.strictEqual(service.get('subject'), service, 'set/get - works for truthy values');
+  assert.strictEqual(service.get('falsy'), '', 'set/get - works for falsy values');
+
+  assert.strictEqual(service.has('subject'), true, 'has - works for truthy values');
+  assert.strictEqual(service.has('falsy'), true, 'has - works for false values');
+
+  service.remove('subject');
+  service.remove('falsy');
+
+  assert.strictEqual(service.has('subject'), false, 'remove - works for truthy values');
+  assert.strictEqual(service.has('falsy'), false, 'remove - works for false values');
+
+  service.set('subject', service);
+  service.set('falsy', '');
+
+  assert.strictEqual(service.get('subject'), service, 'set/get - works for reinstated truthy values');
+  assert.strictEqual(service.get('falsy'), '', 'set/get - works for reinstated falsy values');
 });
