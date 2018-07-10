80ab133634a4d4a334af03c04293c39ada0aceb0
Ben Demboski <ben@turbopatent.com>
2018-07-10 11:02:24 -0700

    Update to ember/ember-cli 3.2

--- app/app.js
+++ app/app.js
@@ -1,15 +1,11 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 import './utils/component-debug';
 
-let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Ember.Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
