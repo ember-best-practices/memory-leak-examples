9d30097610b86a9cb8d9f5175705933ad7082873
Ryan Toronto <ryanto@gmail.com>
2018-07-18 00:25:56 -0400

    Run codemods, fix tests

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
--- tests/integration/components/list-view-test.js
+++ tests/integration/components/list-view-test.js
@@ -1,36 +1,39 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('list-view', 'Integration | Component | list view', {
-  integration: true
-});
+module('Integration | Component | list view', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders a series of items as cards', function(assert) {
-  this.set('items', [{ name: 'foo' }, { name: 'bar' }]);
-  this.render(hbs`
-    {{#list-view items=items as |item|}}
-      <p class="name">{{item.name}}</p>
-    {{/list-view}}
-  `);
+  test('it renders a series of items as cards', async function(assert) {
+    this.set('items', [{ id: 1, name: 'foo' }, { id: 2, name: 'bar' }]);
+    await render(hbs`
+      {{#list-view items=items as |item|}}
+        <p class="name" data-test-id={{item.id}}>{{item.name}}</p>
+      {{/list-view}}
+    `);
 
-  assert.equal(this.$('.name').text().trim(), 'foobar');
-});
+    assert.dom('[data-test-id="1"]').hasText('foo');
+    assert.dom('[data-test-id="2"]').hasText('bar');
+  });
 
-test('it sets up a scroll handler', function(assert) {
-  function scrollHandler() {
-    assert.step('scroll');
-  }
+  test('it sets up a scroll handler', async function(assert) {
+    function scrollHandler() {
+      assert.step('scroll');
+    }
 
-  this.set('items', [{ name: 'foo' }, { name: 'bar' }]);
-  this.set('scrollHandler', scrollHandler);
+    this.set('items', [{ name: 'foo' }, { name: 'bar' }]);
+    this.set('scrollHandler', scrollHandler);
 
-  this.render(hbs`
-    {{#list-view onScroll=scrollHandler items=items as |item|}}
-      <p class="name">{{item.name}}</p>
-    {{/list-view}}
-  `);
+    await render(hbs`
+      {{#list-view onScroll=scrollHandler items=items as |item|}}
+        <p class="name">{{item.name}}</p>
+      {{/list-view}}
+    `);
 
-  window.dispatchEvent(new CustomEvent('scroll'));
+    window.dispatchEvent(new CustomEvent('scroll'));
 
-  assert.verifySteps(['scroll']);
+    assert.verifySteps(['scroll']);
+  });
 });
