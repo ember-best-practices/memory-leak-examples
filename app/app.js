33edb5cdb9ed08ee005fe2e74e5ca6822dc0765e
Ryan Toronto <ryanto@gmail.com>
2018-07-18 00:05:14 -0400

    message

--- app/app.js
+++ app/app.js
@@ -1,15 +1,14 @@
-import Ember from 'ember';
+import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-
 import './utils/component-debug';
 
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
-App = Ember.Application.extend({
+App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
